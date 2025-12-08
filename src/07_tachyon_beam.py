ba16db6a93fecdd6d3deea947c4c1e429e8ca812
Al Matty <al@almatty.com>
2025-12-08 01:33:32 +0100

    Solved 7b with Pascal's triangle

--- src/07_tachyon_beam.py
+++ src/07_tachyon_beam.py
@@ -8,46 +8,66 @@ Plan part 1:
             if current row has "^" and row above has "b":
                 change l & r to "b" (as long as they are ".")
                 increment split counter by 1
+Plan part 2:
+    Pascal's triangle? -> maybe possibilities can be added up as you go down the tree
+    instead of "b", store the number of possible paths at beam points
+    create dict of char indexes per line as keys, n outcomes at this point as values
+    children nodes inherit the sum of their parents' outcomes
+    no split -> no change of possibilities
 """
 
-#in_file = "../data/07_input_test.txt"
 in_file = "../data/07_input.txt"
-part = 1  # 1 or 2
-
 
 # Read data
 with open(in_file) as content:
     lines = content.read().splitlines()
 
-# Part 1
-if part == 1:
-    
-    # Iterate over each char of each line
-    split_counter = 0
-    beam_positions_last_row = []
 
-    for i, line in enumerate(lines):
-        beam_positions_current_row = []
+# Iterate over each char of each line
+split_counter = 0
+# dict with indexes as keys, n possibilities at this point as values
+beam_possibilities_dict_prior = {}
 
-        for j, char in enumerate(line):
+for i, line in enumerate(lines):
 
-            if char == "S":
-                beam_positions_current_row.append(j)
+    # Same dict, but for current row
+    beam_possibilities_dict_current = {}
 
-            if j in beam_positions_last_row:
+    for j, char in enumerate(line):
 
-                if char == ".":
-                    beam_positions_current_row.append(j)
+        if char == "S":  # Set starting value (always 1)
+            beam_possibilities_dict_current[j] = 1
 
-                if char == "^":
-                    split_counter += 1
+        if j in beam_possibilities_dict_prior:
+            # Fetch values from last row/state
+            n_outcomes_prior = beam_possibilities_dict_prior[j]
 
-                    if line[j-1] == ".":  # Add beam to left
-                        beam_positions_current_row.append(j-1)
+            # Case: Normal field: Keep same or add to val from parent
+            if char == ".":
+                if j in beam_possibilities_dict_current:
+                    beam_possibilities_dict_current[j] += n_outcomes_prior
+                else:
+                    beam_possibilities_dict_current[j] = n_outcomes_prior
 
-                    if line[j+1] == ".":  # Add beam to right
-                        beam_positions_current_row.append(j+1)
-        # Update last beam positions
-        beam_positions_last_row = beam_positions_current_row
+            # Case: Splitting field: Add to/set left & right from position
+            if char == "^":
+                split_counter += 1    # Count splits for part 1
+                if line[j-1] == ".":  # Add beam to left
+                    if j-1 in beam_possibilities_dict_current:
+                        beam_possibilities_dict_current[j-1] += n_outcomes_prior
+                    else:
+                        beam_possibilities_dict_current[j-1] = n_outcomes_prior
 
-    print("Part 1 (n splits):", split_counter)
+                if line[j+1] == ".":  # Add beam to right
+                    if j+1 in beam_possibilities_dict_current:
+                        beam_possibilities_dict_current[j+1] += n_outcomes_prior
+                    else:
+                        beam_possibilities_dict_current[j+1] = n_outcomes_prior
+
+    # Overwrite last row info
+    beam_possibilities_dict_prior = beam_possibilities_dict_current 
+
+
+sum_outcomes = sum([v for v in beam_possibilities_dict_current.values()])
+print("\nPart 1 (n splits):", split_counter)
+print("Part 2 (n possible paths):", sum_outcomes)
