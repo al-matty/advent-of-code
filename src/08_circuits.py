b4d6afc29167b3d491437fd8e86dc5b832f12212
Al Matty <al@almatty.com>
2025-12-08 18:39:12 +0100

    Solved 8 with distance matrix

--- src/08_circuits.py
+++ src/08_circuits.py
@@ -12,6 +12,13 @@ Plan part 1:
         update matrix to ignore point pairs that have been handled already
     sort list by set size (lambda len)
     take first 3-item-slice, multiply lengths
+
+Plan part 2:
+    change breaking condition:
+        loop until len(circuits) = 1 AND all points in set
+    final_pair  <- set, always containing last 2 ids handled
+    run loop, look up coords from input via final_pair ids
+    multiply their first coordinates
 """
 
 import numpy as np
@@ -20,6 +27,7 @@ from scipy.spatial import distance_matrix
 
 #in_file = "../data/08_input_test.txt"
 in_file = "../data/08_input.txt"
+part = 2  # 1 or 2
 
 # Read data
 with open(in_file) as content:
@@ -28,7 +36,6 @@ with open(in_file) as content:
 # Create coordinates array
 points = [[int(coord) for coord in line.split(",")] for line in lines]    
 points_arr = np.array(points)
-print(points_arr)
 
 # Create eucl distance matrix
 eucl_distances = distance_matrix(points_arr, points_arr)
@@ -43,10 +50,18 @@ print("\n", already_handled)
 
 # List of sets (circuits are sets)
 circuits = []
-connection_count = 1
+connection_count = 1  # part 1 breaking condition
+final_pair = set()    # part 2 result
 
-# Connect closest pair 1000 times
-while connection_count <= 1000:
+while True:
+    # Part 1: Connect closest pair 1000 times
+    if part == 1 and connection_count > 1000:
+        break
+    # Part 2: Run until one circuit only containing all points
+    if part == 2 and (len(circuits) == 1) and (len(circuits[0]) == len(points)):
+        break
+    print(f"\nIteration {connection_count}")
+    print("circuits:", len(circuits))
 
     # Find closest pair in distances matrix (ignore already handled points)
     masked_eucl_distances = np.ma.array(eucl_distances, mask=already_handled)
@@ -58,6 +73,7 @@ while connection_count <= 1000:
     # Ignore handled ids in future iterations (add to bool mask)
     already_handled[x, y] = True
     already_handled[y, x] = True
+    final_pair = {x, y}
 
     # Find circuit ids of x and y if they exist in any circuit already
     x_circuit_id, y_circuit_id = None, None
@@ -82,7 +98,6 @@ while connection_count <= 1000:
     
     # Case: First id is known only: Add second id to set
     elif x_circuit_id is not None:
-        # x in circuit, y is new - add y
         circuits[x_circuit_id].add(y)
         #print(f"Added {y} to circuit: {circuits[x_circuit_id]}")
 
@@ -93,8 +108,13 @@ while connection_count <= 1000:
 
     connection_count += 1
 
-# Sort "circuits" in-place by len
-circuits.sort(key=lambda x: len(x), reverse=True)
-top_3 = [len(circuit) for circuit in circuits][:3]
-prod_len_top_3 = prod(top_3)
-print("\nPart 1:", prod_len_top_3)
+if part == 1:  # Part 1 solution
+    circuits.sort(key=lambda x: len(x), reverse=True)  # Sort "circuits" in-place by len
+    top_3 = [len(circuit) for circuit in circuits][:3]
+    prod_len_top_3 = prod(top_3)
+    print("\nPart 1:", prod_len_top_3)
+
+else:  # Part 2 solution
+    final_x_coords = [points[_id][0] for _id in final_pair]
+    print("\nfinal_x_coords:", final_x_coords)
+    print("Part 2:", prod(final_x_coords))
